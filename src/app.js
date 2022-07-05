1c588584422aad4f831803c818ca68c286220c16
Annavy11 <ivana.garilovic@gmail.com>
2022-07-05 20:35:21 +0200

    Final finishes
    added footer with link to github repositories,
    temperature unit change enabled

--- src/app.js
+++ src/app.js
@@ -94,34 +94,34 @@ function handleSubmit(event) {
   cityInputElement.value = ``;
 }
 
-/*
+
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
+  let temperatureElement = document.querySelector("#temperature-current");
 
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+  celsiusUnit.classList.remove("active");
+  fahrenheitUnit.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
+  celsiusUnit.classList.add("active");
+  fahrenheitUnit.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature-current");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 let celsiusTemperature = null;
-*/
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-/*
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-*/
+let fahrenheitUnit = document.querySelector("#unit-fahrenheit");
+fahrenheitUnit.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusUnit = document.querySelector("#unit-celsius");
+celsiusUnit.addEventListener("click", displayCelsiusTemperature);
+
 search("New York");
