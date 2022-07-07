e78fecc20ded192e4a602b130a4acc56e1289039
Annavy11 <ivana.garilovic@gmail.com>
2022-07-07 21:01:38 +0200

    Update app.js
    Created a function that's calling for forecast data

--- src/app.js
+++ src/app.js
@@ -58,7 +58,8 @@ function getCurrentPosition(event) {
 let gpsButton = document.querySelector("#gps-button");
 gpsButton.addEventListener("click", getCurrentPosition);
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML ="";
@@ -78,6 +79,13 @@ function displayForecast() {
 
   forecastElement.innerHTML = forecastHTML;
 
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -100,6 +108,9 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   descriptionElement.setAttribute("alt", response.data.weather[0].description);
+
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -146,4 +157,3 @@ let celsiusUnit = document.querySelector("#unit-celsius");
 celsiusUnit.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast()
